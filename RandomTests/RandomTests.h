bfb5990fa52dd35dbba261e0f7bc080261b60929
Valera Antonov <Valera@Valeras-MacBook-Pro.local>
2013-03-27 21:42:55 +0400

    Initial Commit

---
+++ RandomTests/RandomTests.h
@@ -0,0 +1,13 @@
+//
+//  RandomTests.h
+//  RandomTests
+//
+//  Created by Valera Antonov on 3/27/13.
+//  Copyright (c) 2013 Valera Antonov. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface RandomTests : SenTestCase
+
+@end
---
+++ RandomTests/RandomTests.m
@@ -0,0 +1,32 @@
+//
+//  RandomTests.m
+//  RandomTests
+//
+//  Created by Valera Antonov on 3/27/13.
+//  Copyright (c) 2013 Valera Antonov. All rights reserved.
+//
+
+#import "RandomTests.h"
+
+@implementation RandomTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in RandomTests");
+}
+
+@end
